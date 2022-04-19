d5793773b605eca42f86c63dd1da2102895dc9cd
naohatto <naohatto113@gmail.com>
2022-04-19 09:24:10 +0000

    Customer System

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,10 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_user
   end
 
   def edit
+    @customer = current_user
   end
 
   def unsubscribe
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
 
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
@@ -21,16 +21,16 @@ Rails.application.routes.draw do
     get 'orders/complete'
     resources :deliveries, only: [:create, :index, :edit, :update]
   end
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
