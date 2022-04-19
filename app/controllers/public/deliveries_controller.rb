9c5a2182e774354b729dff0dad808283c96656f2
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-04-19 08:31:49 +0000

    配送先一覧画面-編集・削除機能追加

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -9,26 +9,38 @@ class Public::DeliveriesController < ApplicationController
   def create
     @delivery = Delivery.new(delivery_params)
     if @delivery.save
-      flash[:notice] = "配達先を登録しました"
+      flash[:notice] = "配達先情報を登録しました"
       redirect_to public_deliveries_path
     else
       @deliveries = Delivery.all
       render "index"
     end
-
   end
 
   def edit
+    @delivery = Delivery.find(params[:id])
   end
 
   def update
+    @delivery = Delivery.find(params[:id])
+    if @delivery.update
+      flash[:notice] = "配達先情報を編集しました"
+      redirect_to public_deliveries_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    delivery = Delivery.find(params[:id])
+    flash[:notice] = "配送先情報を削除しました。"
+    delivery.destroy
+    redirect_to public_deliveries_path
   end
 
   private
+
 	def delivery_params
-  	params.require(:delivery).permit(:customer_id, :post_code, :address, :address_name)
+   params.require(:delivery).permit(:customer_id, :post_code, :address, :address_name).merge(customer_id: current_customer.id)
   end
 end
