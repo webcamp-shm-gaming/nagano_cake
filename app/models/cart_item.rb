ddb65d45de576cf066fbd49a02f5260a0e5ce1b3
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-04-20 05:54:26 +0000

    アソシエーション設定

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,5 @@
 class CartItem < ApplicationRecord
+  # itemsテーブルとcart_itemsテーブルは1:Nの関係
+  belongs_to :item
+  belongs_to :customer
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  # itemsテーブルとcart_itemsテーブルは1:Nの関係
+  has_many :cart_items, dependent: :destroy
 end
