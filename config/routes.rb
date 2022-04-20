16f72f590149b8e3b684c75cfc765229fe0f1bd5
EC2 Default User <ec2-user@ip-172-31-84-67.ec2.internal>
2022-04-20 08:35:38 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
 
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
@@ -10,27 +10,31 @@ Rails.application.routes.draw do
     get 'homes/top'
   end
 
-  namespace :public do
+  scope module: :public do
     get 'homes/top'
     get 'homes/about'
     resources :customers, only: [:show, :edit, :update]
     get 'customers/unsubscribe'
     resources :items, only: [:index, :show]
-    resources :cart_items, only: [:create, :index, :update, :destroy]
+    resources :cart_items,only: [:index,:update,:create,:destroy] do
+      collection do
+        delete '/' => 'cart_items#destroy_all'
+      end
+    end
     resources :orders, only: [:new, :create, :index, :show]
     get 'orders/complete'
     resources :deliveries, only: [:create, :index, :edit, :update]
   end
-  
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
