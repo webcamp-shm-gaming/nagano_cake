f7f0fc53768edfcde2305f46f5b6844f6cc59184
reszcb740 <reszcb740@Gmail.com>
2022-04-21 08:05:01 +0000

    Add model items genres

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,3 @@
 class Genre < ApplicationRecord
-  belongs_to :item
+  belongs_to :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
-　has_many :genre_id
+　has_many :genres, dependent: :destroy
+　belongs_to :cart_item_id
   def price
     (self.price * 1.10).round
   end
