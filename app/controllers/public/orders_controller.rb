4724758205297783bd4bbaf6e3a63e089843c8ed
naohatto <naohatto113@gmail.com>
2022-04-25 14:32:50 +0000

    Modified Order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -51,9 +51,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = current_customer.orders.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def complete
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   enum pay_type: { credit_card: 0, transfer: 1 }
 
   belongs_to :customer
+  has_many :order_details
 
   # 小計を求めるメソッド
   def subtotal
