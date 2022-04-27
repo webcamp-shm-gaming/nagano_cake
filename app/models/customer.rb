0cb62084da9669ab9bd87c0c51cd76acea4b74a3
naohatto <naohatto113@gmail.com>
2022-04-27 04:28:09 +0000

    japanese

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,9 +6,7 @@ class Customer < ApplicationRecord
 
   has_many :cart_items, dependent: :destroy
 
-<<<<<<< HEAD
-  validates :first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :phone_number, :address, presence: true
-=======
   has_many :orders, dependent: :destroy
->>>>>>> origin/develop
+
+  validates :first_name, :last_name, :first_name_kana, :last_name_kana, :post_code, :phone_number, :address, presence: true
 end
